7c2de6878cd6492a6aa791b13f900f20652a6afe
wiiggs <47868798+wiiggs@users.noreply.github.com>
2019-04-27 10:52:56 -0500

    We will neeed the timer ticked event for temperature changes

---
+++ GP3/src/states/FanState.java
@@ -0,0 +1,17 @@
+package states;
+
+public class FanState extends TemperatureControllerState {
+
+	@Override
+	public void enter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void leave() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
